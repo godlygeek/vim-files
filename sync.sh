967a1946c2284eed375393cee4a67408d3d13441
Matt Wozniski <mjw@drexel.edu>
2009-12-20 06:21:31 -0500

    Add a script to pull or push from all subtrees

---
+++ sync.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+. $(git --exec-path)/git-sh-setup
+
+require_work_tree
+
+if [ x"$1" = xpull ]; then
+  echo "pulling from origin"
+  git pull
+
+  git remote -v | sed 's/\t.*//' | while read remote; do
+    if [ x"$remote" != xorigin ]; then
+      echo "pulling from $remote"
+      git subtree merge --prefix="runtimes/$remote" "$remote/master"
+    fi
+  done
+fi
+
+if [ x"$1" = xpush ]; then
+  echo "pushing to origin"
+  git push
+
+  git remote -v | sed 's/\t.*//' | while read remote; do
+    if [ x"$remote" != xorigin ]; then
+      echo "pushing to $remote"
+      git push "$remote" "$(git subtree split --prefix="runtimes/$remote"):master"
+    fi
+  done
+fi
